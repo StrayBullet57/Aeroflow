92f1d4ff210c3ba6e51a894714b0f82bce407488
Raymundo Alcontin <pinkeyebagsray@gmail.com>
2026-05-05 09:08:32 +0800

    feat(gui): add homepage UI

---
+++ main.java
@@ -0,0 +1,10 @@
+import javax.swing.SwingUtilities;
+
+import src.GUI.Homepage;
+
+public class main{
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Homepage().createUI());
+    }
+
+}
