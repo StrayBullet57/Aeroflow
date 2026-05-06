d9dc993fb0945d10a99e1913587d1d7bab4e6870
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-06 15:44:40 +0800

    feat: add fields and methods to class

---
+++ Models/Route.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Route{
+    private String routeID;
+    private String origin;
+    private String destination;
+    private int duration;
+
+    public void select(){
+
+    }
+}
