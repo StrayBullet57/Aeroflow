b4c3008fbc3714a61e1517c4974d03ec7e125636
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-08 01:08:09 +0800

    feat: add variables and constructor for Flight

--- models/Flight.java
+++ models/Flight.java
@@ -4,3 +4,22 @@
 //destination: String
 //price: Double
 //status: String
+package models;
+
+public class Flight{
+    private String flightID;
+    private Airline airline;
+    private Route route;
+    private Double price;
+    private String status;
+
+    public Flight(String flightID, Airline airline, Route route, Double price, String status){
+        this.flightID = flightID;
+        this.airline = airline;
+        this.route = route;
+        this.price = price;
+        this.status = status;
+    }
+
+
+}
