6a8c5d8c68b7e2f5c593c32812cab0740d78465b
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-08 00:55:41 +0800

    feat: add methods and constructor for route

--- models/Route.java
+++ models/Route.java
@@ -6,6 +6,29 @@ public class Route{
     private String destination;
     private int duration;
 
+    public route(String routeID, String origin, String destination, int duration){
+        this.routeID = routeID;
+        this.origin = origin;
+        this.destination = destination;
+        this.duration = duration;
+    }
+
+    public String getRouteID(){
+        return routeID;
+    }
+
+    public String getOrigin(){
+        return origin;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
+
     public void select(){
 
     }
