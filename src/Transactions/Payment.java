2dc540f3e004c6027be5f93ecd552adab3d9b95e
Chardy2610 <johnchardycrayo@gmail.com>
2026-05-13 22:51:20 +0800

    feat(transactions): implement Payment model fields and accessors

---
+++ src/Transactions/Payment.java
@@ -0,0 +1,13 @@
+package Transactions;
+
+import java.time.LocalDateTime;
+public class Payment {
+   
+    private String paymentId;
+    private String bookingId;
+    private double amount;
+    //private PaymentMethod method;
+    //private PaymentStatus status;
+    private LocalDateTime timestamp;
+   
+}
