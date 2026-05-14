58323360982aa5faf148fef03645cb271e947ac3
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-15 02:06:14 +0800

    feat: add constructor, methods, and variables

---
+++ src/models/Seat.java
@@ -0,0 +1,32 @@
+package models;
+
+public class Seat {
+    private String seatID;
+    private String seatNumber;
+    private String seatClass;
+    private boolean isAvailable;
+
+    public Seat(String seatID, String seatNumber, String seatClass) {
+        this.seatID = seatID;
+        this.seatNumber = seatNumber;
+        this.seatClass = seatClass;
+        this.isAvailable = true;
+    }
+
+    public boolean isAvailable(){
+        return isAvailable;
+    }
+
+    public void bookSeat(){
+        isAvailable = false;
+    }
+
+    public String getSeatNumber(){
+        return seatNumber;
+    }
+
+    @Override
+    public String toString(){
+        return seatNumber + " - " + seatClass + (isAvailable ? " (Available)" : " (Taken)");
+    }
+}
