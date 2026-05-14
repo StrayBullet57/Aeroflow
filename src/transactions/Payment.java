78989bfcfbc47e456eee55f576d512cfe493e811
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-15 02:05:13 +0800

    feat: add constructor, methods

---
+++ src/transactions/Payment.java
@@ -0,0 +1,27 @@
+package transactions;
+public class Payment {
+   
+    private String paymentID;
+    private String bookingID;
+    private double amount;
+    //private PaymentMethod method;
+    //private PaymentStatus status;
+    private String paymentDate;
+
+    public Payment(String paymentID, String bookingID, double amount, String paymentDate){
+        this.paymentID = paymentID;
+        this.bookingID = bookingID;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+    }
+
+    public boolean processPayment(){
+        System.out.println("Payment Successful.");
+        return true;
+    }
+    @Override
+    public String toString(){
+        return "Payment ID: " + paymentID + " Amount: " + amount;
+    }
+   
+}
