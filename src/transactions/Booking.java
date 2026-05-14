3247df1dee6b830090402d8a4d6fb0320b8109e4
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-15 02:05:46 +0800

    feat: add constructor, methods

---
+++ src/transactions/Booking.java
@@ -0,0 +1,49 @@
+package transactions;
+
+import java.util.ArrayList;
+import models.Flight;
+import models.Seat;
+
+public class Booking{
+    private String bookingID;
+    private Flight flight;
+    private Seat seat;
+    private Payment payment;
+    private String status;
+
+    private static ArrayList<Booking> bookingHistory = new ArrayList<>();
+
+    public Booking(String bookingID, Flight flight, Seat seat, Payment payment){
+        this.bookingID = bookingID;
+        this.flight = flight;
+        this.seat = seat;
+        this.payment = payment;
+        this.status = "Pending";
+    }
+
+    public void confirmBooking(){
+        if(seat.isAvailable() && payment.processPayment()){
+            seat.bookSeat();
+            status = "Confirmed";
+            bookingHistory.add(this);
+            System.out.println("Booking Confirmed.");
+        } else{
+            status = "Failed";
+            System.err.println("Booking Failed.");
+        }
+    }
+
+    public static void viewHistory(){
+        System.out.println("Booking History");
+
+        for(Booking booking : bookingHistory){
+            System.out.println(booking);
+            System.out.println();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "Booking ID: " + bookingID + "\nFlight: " + flight.getFlightID() + "\nSeat: " + seat.getSeatNumber() + "\nStatus: " + status;
+    }
+}
