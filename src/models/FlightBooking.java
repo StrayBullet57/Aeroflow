01cc947e30373cb460a1771686b573eda591ef49
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-15 02:09:03 +0800

    Feat: add class to interact with other class and methods

---
+++ src/models/FlightBooking.java
@@ -0,0 +1,45 @@
+package models;
+
+import transactions.Booking;
+import transactions.Payment;
+import java.util.ArrayList;
+
+public class FlightBooking{
+
+    private ArrayList<Flight> flights;
+    private ArrayList<Booking> bookings;
+
+    public FlightBooking(){// create arraylist
+        flights = new ArrayList<>();
+        bookings = new ArrayList<>();
+    }
+
+    public void addFlight(Flight flight){// add flight to arraylist
+        flights.add(flight);
+    }
+
+    public ArrayList<Flight> getFlights(){// return flights, pede magamit sa show flight lists
+        return flights;
+    }
+
+    public Flight findFlight(String flightID){// find flight loop, katung search flightID na feature
+        for (Flight f : flights) {
+            if (f.getFlightID().equals(flightID)) {
+                return f;
+            }
+        }
+        return null;
+    }
+
+    public Booking createBooking(String bookingID, Flight flight, Seat seat, double amount){
+
+        Payment payment = new Payment("PMNT-" + bookingID, bookingID, amount, "2026-05");// create payment
+        Booking booking = new Booking(bookingID, flight, seat, payment);// create booking
+        bookings.add(booking);// store booking
+        return booking;
+    }
+
+    public ArrayList<Booking> getBookings(){// return bookings, show all bookings list, para atong booking history na feature.
+        return bookings;
+    }
+}
