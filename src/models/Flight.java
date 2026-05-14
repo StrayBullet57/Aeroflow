e4363522ca59ac6f6034479b083223c2dc4b9d8c
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-15 02:02:25 +0800

    feat: add constructor, methods

---
+++ src/models/Flight.java
@@ -0,0 +1,44 @@
+package models;
+import java.util.ArrayList;
+
+public class Flight{
+    private String flightID;
+    private Airline airline;
+    private Route route;
+    private double price;
+    private String status;
+    private ArrayList<Seat> seats;
+
+    public Flight(String flightID, Airline airline, Route route, double price, String status){
+        this.flightID = flightID;
+        this.airline = airline;
+        this.route = route;
+        this.price = price;
+        this.status = status;
+        this.seats = new ArrayList<>();
+    }
+    
+    public ArrayList<Seat> getSeats(){
+        return seats;
+    }
+
+    public void addSeat(Seat seat){
+        seats.add(seat);
+    }
+
+    public String getFlightID(){
+        return flightID;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+    @Override
+    public String toString(){
+        return "Flight: " + flightID + "\nAirline: " + airline + "\nRoute: " + route + "\nPrice: " + price + "\nStatus: " + status;
+    }
+}
