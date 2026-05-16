c42d57c7d44699fa368c79b77951ecce28ebc11d
Chardy2610 <johnchardycrayo@gmail.com>
2026-05-16 17:33:53 +0800

    test(main): Testing userinput for payment.java

--- src/main.java
+++ src/main.java
@@ -5,20 +5,11 @@ import transactions.Payment;
 public class main{
     public static void main(String[] args) {
 
-        System.out.println("--- RUNNING AEROFLOW PAYMENT TEST ---");
-
-        Payment testPayment1 = new Payment("PMT-001", "BK-9982", 4500.50, Payment.PaymentMethod.GCASH);
-
-        testPayment1.processPayment();
-
-        System.out.println(testPayment1);
-        System.out.println();
-
-        Payment testPayment2 = new Payment("PMT-002", "BK-1104", -50.0, Payment.PaymentMethod.CREDIT_CARD);
-        testPayment2.processPayment();
-        System.out.println(testPayment2);
-
-        System.out.println("--- END OF PAYMENT TEST, LAUNCHING GUI ---\n");
+        Payment userPayment = Payment.createPaymentFromInput();
+        
+        userPayment.processPayment();
+        System.out.println(userPayment);
+        System.out.println("\n=== CLOSING PAYMENT TEST, LAUNCHING SYSTEM GUI ===\n");
 
         SwingUtilities.invokeLater(() -> new MainContent().start());
     }
