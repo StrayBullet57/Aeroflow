d4c6819134131d9743cf201d98685cb0aa6a6602
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-21 02:14:17 +0800

    feat: add data to location list

--- src/datas/LocationData.java
+++ src/datas/LocationData.java
@@ -2,7 +2,6 @@ package datas;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import models.Location;
 
 public class LocationData {
@@ -10,9 +9,11 @@ public class LocationData {
     public static List<Location> getLocations() {
         List<Location> locations = new ArrayList<>();
         Location Cebu = new Location("Cebu", 5,10);
-        Location Manila = new Location("Cebu", 5,10);
+        Location Manila = new Location("Manila", 5,10);
+        Location Davao = new Location("Davao",5,10);
         locations.add(Cebu);
         locations.add(Manila);
+        locations.add(Davao);
 
         return locations;
     }
