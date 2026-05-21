11d4867a8a768cc65f069d4a2a66bd603884bc8a
StrayBullet57 <leunamednamzug@gmail.com>
2026-05-21 12:37:45 +0800

    refactor(ProfilePage): fiixed error

--- src/GUI/ProfilePage.java
+++ src/GUI/ProfilePage.java
@@ -187,7 +187,7 @@ public class ProfilePage extends JFrame {
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(() -> {
-            new AeroFlowProfileUI();
+            new ProfilePage();
         });
     }
 }
