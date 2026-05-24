95486e693ee075f79250e8c0bc85ed112784b63e
eddvergil <eddvergil.banzon@cit.edu>
2026-05-24 11:44:24 +0800

    make the text box rounded

--- src/GUI/Profile.java
+++ src/GUI/Profile.java
@@ -59,8 +59,15 @@ public class Profile extends JPanel {
 
         profileCard.setBackground(new Color(15, 32, 65));
 
-        profileCard.setBorder(new EmptyBorder(30, 30, 30, 30));
+        profileCard.setBorder(BorderFactory.createCompoundBorder(
+        new RoundedBorder(30, new Color(0, 153, 255)),
+        new EmptyBorder(30, 30, 30, 30)
 
+        formCard.setBorder(BorderFactory.createCompoundBorder(
+        new RoundedBorder(30, new Color(0, 153, 255)),
+        new EmptyBorder(30, 30, 30, 30)
+));
+));
         // PROFILE PICTURE
         JLabel profilePic = new JLabel();
 
@@ -268,4 +275,55 @@ public class Profile extends JPanel {
 
         return output;
     }
+    class RoundedBorder implements javax.swing.border.Border {
+
+    private int radius;
+    private Color color;
+
+    RoundedBorder(int radius, Color color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius + 1, radius + 1, radius + 1, radius + 1);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return false;
+    }
+
+    @Override
+    public void paintBorder(
+            Component c,
+            Graphics g,
+            int x,
+            int y,
+            int width,
+            int height
+    ) {
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON
+        );
+
+        g2.setColor(color);
+
+        g2.setStroke(new BasicStroke(2));
+
+        g2.drawRoundRect(
+                x + 1,
+                y + 1,
+                width - 3,
+                height - 3,
+                radius,
+                radius
+        );
+    }
+}
 }
